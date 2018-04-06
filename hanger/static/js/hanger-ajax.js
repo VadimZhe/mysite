4124fd49aa1efd4a10162cb4c8d0ab3731ffa8e4
Vadim Zherdev <vadim.zherdev@gmail.com>
2018-04-06 23:06:59 +0300

    Trying AJAX with PyCharm

--- hanger/static/js/hanger-ajax.js
+++ hanger/static/js/hanger-ajax.js
@@ -1,6 +1,5 @@
 $('#show_it').click(function(){
     $.get('/hanger/showme/', {0}, function(data){
-               console.log("AJAX starting...")
                $('#the_object"').html("Here I am!");
     });
 });
